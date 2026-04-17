e520ca1d3ed31d91f73c01389ff7948f781c9615
deepakushwaha17 <deepakushwaha155@gmail.com>
2026-04-17 12:06:10 +0530

    [Deepa Kushwaha] updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import ProtectedRoute from './components/layout/ProtectedRoute';
